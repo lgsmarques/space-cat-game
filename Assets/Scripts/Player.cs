8ffd0e0078883ab754b04f81f7d5dd512427626d
lgsmarques <luizgsmarques@gmail.com>
2024-08-20 20:08:29 -0300

    init project

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+
+    public float speed = 10f;
+    public float forceJump = 20f;
+    public Vector2 friction = new Vector2(.1f, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+        HandleJump();
+    }
+
+    private void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
+            //rigidbody2D.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+            //rigidbody2D.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
+        else if (myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity += friction;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            myRigidbody.velocity = Vector2.up * forceJump;
+        }
+    }
+}
