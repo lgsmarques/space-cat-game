c156b35851975b141c67bc8efb67b2b83d26311b
JonatasWillian <91629214+JonatasWillian@users.noreply.github.com>
2024-08-21 18:14:40 -0300

    Update Player.cs
    retirado as linhas comentadas e acrescentado a funcao IsGrounded para o player pular apenas uma vez

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,24 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
+    [Header("Rigidbody")]
     public Rigidbody2D myRigidbody;
 
+    [Header("Movement")]
     public float speed = 10f;
     public float forceJump = 20f;
     public Vector2 friction = new Vector2(.1f, 0);
 
-    void Start()
+    [Header("Animation Player")]
+    public float playerSwipeDuration = .1f;
+
+    [Header("Jump Check")]
+    public Collider2D collider2D;
+    public float disToGround;
+    public float spaceToGround = .1f;
+
+
+    private void Awake()
     {
-        
+        if (collider2D != null)
+        {
+            disToGround = collider2D.bounds.extents.y;
+        }
     }
-    
     void Update()
     {
+        IsGrounded();
         HandleMovement();
         HandleJump();
+    }
+
+    private bool IsGrounded()
+    {
+        Debug.DrawRay(transform.position, -Vector2.up, Color.magenta, disToGround + spaceToGround);
+        return Physics2D.Raycast(transform.position, -Vector2.up, disToGround + spaceToGround);
     }
 
     private void HandleMovement()
@@ -26,12 +47,18 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
-            //rigidbody2D.transform.localScale = new Vector3(-1, 1, 1);
+            if (myRigidbody.transform.localScale.x != -1)
+            {
+                myRigidbody.transform.DOScaleX(-1, playerSwipeDuration);
+            }
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
-            //rigidbody2D.transform.localScale = new Vector3(1, 1, 1);
+            if (myRigidbody.transform.localScale.x != 1)
+            {
+                myRigidbody.transform.DOScaleX(1, playerSwipeDuration);
+            }
         }
 
         if (myRigidbody.velocity.x > 0)
@@ -46,7 +73,7 @@ public class Player : MonoBehaviour
 
     private void HandleJump()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && IsGrounded())
         {
             myRigidbody.velocity = Vector2.up * forceJump;
         }
