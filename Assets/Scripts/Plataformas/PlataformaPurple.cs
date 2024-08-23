ff634a5a5904d302ab1588ffe786a430d4d480cf
JonatasWillian <91629214+JonatasWillian@users.noreply.github.com>
2024-08-22 22:15:59 -0300

    tirado algumas linhas de códigos que não estavam sendo usadas

--- Assets/Scripts/Plataformas/PlataformaPurple.cs
+++ Assets/Scripts/Plataformas/PlataformaPurple.cs
@@ -4,21 +4,10 @@ using UnityEngine;
 
 public class PlataformaPurple : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
     public GameObject plataforma;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Plataforma verde");
         plataforma.SetActive(false);
     }
 }
